144bea6b5b7e5a89a5c1a5e4177b48d12731ca03
Kevin <kevinmenar1993@gmail.com>
2014-08-05 23:30:24 -0600

    Tarea #1 Parte Final
    Todo Listo y en orden

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Analisis-GR40
 =============
-
+Para la ejecución del presente programa unicamente es abrir el archivo Test.py con el Idle python y presionar F5, de esta manera espezara a ejecutarse automaticamente hasta llegar a los 50000 elementos, este proceso puede tardar varios minutos y unicamente se muestran los tiempos de ejecución
 Tareas
--- Test.py
+++ Test.py
@@ -3,18 +3,38 @@ import random
 
 ## random.randint(0, 10000)
 
-def OdeN ():
+def OdeN (Var):
     i = 0
     Lista = []
-    while i<50000:
-        Lista = Lista + [50000-i]
+    while i<Var:
+        Lista = Lista + [i]
         i = i + 1
+    print ("#######################################")
+    print ("#Tiempo de ejecucion de forma Acedente#")
+    print ("#######################################")
+    BubbleSort (Lista)
+    SelectionSort (Lista)
+    while i<Var:
+        Lista = Lista + [random.randint(0, Var)]
+        i = i + 1
+    print ("########################################")
+    print ("#Tiempo de ejecucion de forma Aleatoria#")
+    print ("########################################")
+    BubbleSort (Lista)
+    SelectionSort (Lista)
+    while i<Var:
+        Lista = Lista + [Var-i]
+        i = i + 1
+    print ("#########################################")
+    print ("#Tiempo de ejecucion de forma Decendente#")
+    print ("#########################################")
     BubbleSort (Lista)
     SelectionSort (Lista)
         
         
 
 def BubbleSort (Lista):
+    print ("Funcion del BubbleSort")
     print("Minutos inicial: " + time.strftime("%M"))
     print("Segundo Inicial: " + time.strftime("%S"))
     LenList = len(Lista)
@@ -26,9 +46,11 @@ def BubbleSort (Lista):
                 Lista[j+1] = Aux
     print("Minutos Final: " + time.strftime("%M"))
     print("Segundo Final: " + time.strftime("%S"))
-    ##print (Lista)
+    print("_______________________")
+
 
 def SelectionSort (Lista):
+    print ("Funcion del SelectionSort")
     print("Minutos inicial: " + time.strftime("%M"))
     print("Segundo Inicial: " + time.strftime("%S"))
     LenList = len(Lista)
@@ -42,6 +64,17 @@ def SelectionSort (Lista):
         Lista[temp] = Aux
     print("Minutos Final: " + time.strftime("%M"))
     print("Segundo Final: " + time.strftime("%S"))
-    ##print (Lista)
+    print("_______________________")
 
-OdeN ()
+
+def Ejecutable ():
+    Var = 1
+    while Var <= 5:
+        print("-----------------------------------------")
+        print("Cantidad de elementos en la lista: " + str(Var))
+        print("-----------------------------------------")
+        OdeN (Var*10000)
+        Var = Var + 1
+    print ("Fin Del Programa")
+
+Ejecutable ()
